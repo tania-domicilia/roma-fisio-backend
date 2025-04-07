c0516b9f790ace4344ca3c71f7a4f6fb539534d1
tania-domicilia <apreafederico@gmail.com>
2025-04-07 16:12:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 👇 Aggiungi questa parte
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://clinquant-hummingbird-be10bb.netlify.app"],  # o ["*"] per tutti
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
